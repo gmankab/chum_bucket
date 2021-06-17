41732bb8ea77d16112aa7e887fae2987793842bd
gmanka <gmankab@gmai.com>
2021-06-17 07:05:30 +0300

    aboba

--- chum_bucket/secret.py
+++
@@ -1,5 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8=cth@3efdgfdgfaboba.aboba.aboba.wiedfefwefiewfueifie'
-
-login = 'gmanka'
-password = 'gayorgy'
